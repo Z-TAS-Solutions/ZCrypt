b8fa3f1cfd0e6f5755d6e9234be3fad1c91db86a
zischl <supermechs456@gmail.com>
2026-03-07 14:46:09 +0530

    Trying out go lang aes gcm libs, output working as expected

--- ZCrypt-Go/main.go
+++ ZCrypt-Go/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"fmt"
+	"io"
 )
 
 func main() {
 	fmt.Println("meow !!")
+
+	text := []byte("hellow there !")
+	key := []byte("..whatifthekeyisalsohellowthere!")
+
+	ZCipher, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gcm, err := cipher.NewGCM(ZCipher)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(gcm.Seal(nonce, nonce, text, nil))
+
 }
