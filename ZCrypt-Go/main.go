d7893ea66fc96370cfc191a0e944c6aecfd0cc96
zischl <supermechs456@gmail.com>
2026-03-08 00:00:46 +0530

    - Extended encryption flow to operate directly on data file - CLI now supports -e <filename> and -d <filename> for encryption and decryption - Added serialization and deserialization for JSON inputs and outputs

--- ZCrypt-Go/main.go
+++ ZCrypt-Go/main.go
@@ -2,40 +2,83 @@ package main
 
 import (
 	"ZCrypt/internal/CrypticEngine"
-	"crypto/rand"
+	"encoding/json"
+	"flag"
 	"fmt"
-	"io"
 	"os"
 )
 
 func main() {
-	FilePath := os.Args[1]
 
-	record := CrypticEngine.CrypticRecord{
-		SchemaVersion: 999,
-		UserID:        "45567889",
-		TemplateID:    "753468",
-		TemplateType:  "Fusion",
-		TemplateVer:   1,
+	KEK := []byte("Q7mZp2Hk9Vt4sFb1Rj8NcW3yLd6XgA0g")
+
+	encryptFlag := flag.String("e", "", "encrypt mode")
+	decryptFlag := flag.String("d", "", "decrypt mode")
+
+	flag.Parse()
+
+	if *encryptFlag != "" {
+
+		record := CrypticEngine.CrypticRecord{
+			SchemaVersion: 999,
+			UserID:        "45567889",
+			TemplateID:    "753468",
+			TemplateType:  "Fusion",
+			TemplateVer:   1,
+		}
+
+		FilePath := *encryptFlag
+
+		Data, err := os.ReadFile(FilePath)
+		if err != nil {
+			fmt.Println("check the damned file ffs", err)
+			os.Exit(1)
+		}
+
+		record.Ciphertext = CrypticEngine.Encrypt(&record, KEK, Data)
+
+		fmt.Println("Nounce: ", record.TemplateNonce)
+		fmt.Println("Ciphertext: ", record.Ciphertext)
+		fmt.Println("DEK: ", record.DEK)
+		fmt.Println("WrapNounce: ", record.WrapNonce)
+
+		Output, err := json.MarshalIndent(record, "", " ")
+		if err != nil {
+			fmt.Println("Serialization Failure : ", err)
+			os.Exit(1)
+
+		}
+
+		OutPath := FilePath + ".json"
+
+		if err := os.WriteFile(OutPath, Output, 0600); err != nil {
+			fmt.Println("write error:", err)
+			os.Exit(1)
+		}
+
 	}
 
-	Data, err := os.ReadFile(FilePath)
-	if err != nil {
-		fmt.Println("check the damned file ffs", err)
-		os.Exit(1)
+	if *decryptFlag != "" {
+
+		FilePath := *decryptFlag
+
+		Data, err := os.ReadFile(FilePath)
+		if err != nil {
+			fmt.Println("check the damned file ffs", err)
+			os.Exit(1)
+		}
+
+		var record CrypticEngine.CrypticRecord
+
+		json.Unmarshal(Data, &record)
+
+		fmt.Println("Nounce: ", record.TemplateNonce)
+		fmt.Println("Ciphertext: ", record.Ciphertext)
+		fmt.Println("DEK: ", record.DEK)
+		fmt.Println("WrapNounce: ", record.WrapNonce)
+
+		fmt.Println(string(CrypticEngine.Decrypt(&record, KEK)))
+
 	}
 
-	KEK := make([]byte, CrypticEngine.DEKSize)
-	if _, err := io.ReadFull(rand.Reader, KEK); err != nil {
-		fmt.Println(err)
-	}
-
-	record.Ciphertext = CrypticEngine.Encrypt(&record, KEK, Data)
-
-	fmt.Println("Nounce: ", record.TemplateNonce)
-	fmt.Println("Ciphertext: ", record.Ciphertext)
-	fmt.Println("DEK: ", record.DEK)
-	fmt.Println("WrapNounce: ", record.WrapNonce)
-
-	fmt.Println(string(CrypticEngine.Decrypt(&record, KEK)))
 }
